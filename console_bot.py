b9774e28e355f2432f9414ba911a0d5f83be6126
Julia Zahorovska <ju.zagorovsky@gmail.com>
2025-10-25 22:09:55 +0200

    Add decorator for exceptions in console_bot.py

--- console_bot.py
+++ console_bot.py
@@ -1,15 +1,40 @@
+MESSAGES = {
+    ValueError: "Please provide 2 arguments: name and phone number",
+    IndexError: "Please enter a name after the command. Usage: phone <name>",
+    TypeError: "Invalid command format or wrong number of arguments",
+}
+
+
+# Decorator for exceptions
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except Exception as e:
+
+            for e_type, msg in MESSAGES.items():
+                if isinstance(e, e_type):
+                    return msg
+            return f"Unexpected error: {e}"
+
+    return inner
+
+
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
 
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 
+@input_error
 def change_contact(args, contacts):
     name, new_phone = args
     if name in contacts.keys():
@@ -19,7 +44,9 @@ def change_contact(args, contacts):
         return "Contact was not found"
 
 
+@input_error
 def show_phone(args, contacts):
+
     name = args[0]
     if name in contacts.keys():
         return f"{contacts[name]}"
@@ -27,6 +54,7 @@ def show_phone(args, contacts):
         return "Contact was not found"
 
 
+@input_error
 def show_all(contacts):
     return contacts
 
